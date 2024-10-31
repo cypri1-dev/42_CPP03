74b636f0918464a02717a1d8401081880a842058
Cyprien Ferreira <cyferrei@paul-f4Br4s8.clusters.42paris.fr>
2024-10-31 17:29:37 +0100

    [ALL_DONE]: tests OK - no leaks

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,154 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 13:23:56 by cyferrei          #+#    #+#             */
+/*   Updated: 2024/10/31 17:28:06 by cyferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include <climits>
+
+/*--------------------------------------------------------ORTHODOX CANONICAL FORM---------------------------------------------------------*/
+
+ClapTrap::ClapTrap() {
+	std::cout << BOLD_ON GREEN << "Default constructor called!" << RESET << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string const name) : _name(name), _hitPoint(10), _energyPoint(10), _attackDamage(0) {
+	std::cout << BOLD_ON YELLOW << "Custom constructor called!" << RESET << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other) {
+	std::cout << BOLD_ON YELLOW << "Copy constructor called!" << RESET << std::endl;
+	*this = other;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &other) {
+	std::cout << BOLD_ON YELLOW << "Copy assigment called!" << RESET << std::endl;
+	if (this != &other)
+	{
+		this->_name = other.getName();
+		this->_hitPoint = other.getHitPoint();
+		this->_energyPoint = other.getEnergyPoint();
+		this->_attackDamage = other.getAttackDamage();
+	}
+	return (*this);
+}
+
+ClapTrap::~ClapTrap() {
+	std::cout << BOLD_ON RED << "Destructor called!" << RESET << std::endl;
+}
+
+/*-----------------------------------------------------GETRER - SETTER -----------------------------------------------------------------*/
+
+std::string ClapTrap::getName(void)const {
+	return (this->_name);
+}
+
+unsigned int ClapTrap::getHitPoint(void)const {
+	return (this->_hitPoint);
+}
+
+unsigned int ClapTrap::getEnergyPoint(void)const {
+	return (this->_energyPoint);
+}
+
+unsigned int ClapTrap::getAttackDamage(void)const {
+	return (this->_attackDamage);
+}
+
+void ClapTrap::setHitPoint(unsigned int value){
+	this->_hitPoint = value;
+}
+
+void ClapTrap::setEnergyPoint(unsigned int value) {
+	this->_energyPoint = value;
+}
+
+void ClapTrap::actionWrapper(unsigned int value, int func) {
+	switch (func) {
+		case(ATTACK):
+			this->attack("Brakis");
+			break;
+		case(TAKEDAMAGE):
+			this->takeDamage(value);
+			break;
+		case(REPAIRED):
+			this->beRepaired(value);
+			break;
+		default:
+			std::cout << "No action!" << std::endl;
+	}
+}
+
+/*---------------------------------------------------------FUNCTIONS----------------------------------------------------------------------*/
+
+void ClapTrap::run(void) {	
+	std::cout << BOLD_ON << "My name is " << this->getName() << RESET << std::endl;
+	std::cout << BOLD_ON << "Hit point --> " << this->getHitPoint() << RESET << std::endl;
+	std::cout << BOLD_ON << "Energy point --> " << this->getEnergyPoint() << RESET << std::endl;
+	std::cout << BOLD_ON << "Attack damage --> " << this->getAttackDamage() << RESET << std::endl;
+	
+	std::srand(std::time(0));
+	while(this->getEnergyPoint() > 0 && this->getHitPoint() > 0)
+	{
+		unsigned int randomValue = (std::rand() % 10) + 1;
+		unsigned int randomAction = (std::rand() % 3) + 1;
+		
+		std::cout << BOLD_ON << "-----------------------------" << RESET << std::endl;
+		this->actionWrapper(randomValue, randomAction);
+		std::cout << BOLD_ON << "Hit point --> " << this->getHitPoint() << RESET << std::endl;
+		std::cout << BOLD_ON << "Energy point --> " << this->getEnergyPoint() << RESET << std::endl;
+		std::cout << BOLD_ON << "Attack damage --> " << this->getAttackDamage() << RESET << std::endl;
+		sleep(2);
+	}
+}
+
+void ClapTrap::attack(const std::string &target) {
+	unsigned int oldEnergyPoint = this->getEnergyPoint();
+
+	if (this->getEnergyPoint() == 0)
+	{
+		std::cout << BOLD_ON RED << "No more energy! Can't do anything!" << RESET << std::endl;
+		return;
+	}
+	this->setEnergyPoint(oldEnergyPoint - 1);
+	std::cout << "Claptrap " << BOLD_ON BLUE << this->getName() << RESET << " attacks " << BOLD_ON YELLOW << target << RESET << ", causing " << BOLD_ON RED << this->getAttackDamage() << RESET << " points of damage!" << std::endl;
+	
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+	unsigned int oldHitpoint = this->getHitPoint();
+	
+	if (amount >= oldHitpoint) {
+		std::cout << BOLD_ON RED << "No more hit points!" << RESET << std::endl;
+		this->setHitPoint(0);
+	}
+	else {
+		this->setHitPoint(oldHitpoint - amount);
+		std::cout << "Claptrap " << BOLD_ON BLUE << this->getName() << RESET << " took " << RED << amount << RESET << " damage" << std::endl;
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+	unsigned int oldHitPoint = this->getHitPoint();
+	unsigned int maxHitPoint = 10;
+	unsigned int oldEnergyPoint = this->getEnergyPoint();
+	
+	if (this->getEnergyPoint() == 0)
+	{
+		std::cout << BOLD_ON RED << "No more energy! Can't do anything!" << RESET << std::endl;
+		return;
+	}
+	this->setEnergyPoint(oldEnergyPoint - 1);
+	std::cout << "Claptrap " << BOLD_ON BLUE << this->getName() << RESET << " repaired " << RED << amount << RESET << " hit points!" << std::endl;
+	if (oldHitPoint + amount > maxHitPoint)
+		this->setHitPoint(10);
+	else
+		this->setHitPoint(oldHitPoint + amount);
+}
---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 13:17:32 by cyferrei          #+#    #+#             */
+/*   Updated: 2024/10/31 16:56:06 by cyferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+
+#define BOLD_ON "\033[1m"
+#define BOLD_OFF "\033[0m"
+#define YELLOW "\033[33m"
+#define RESET "\033[0m"
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define BLUE "\033[34m"
+
+#define ATTACK 1
+#define TAKEDAMAGE 2
+#define REPAIRED 3
+
+class ClapTrap {
+	private:
+		std::string _name;
+		unsigned int _hitPoint;
+		unsigned int _energyPoint;
+		unsigned int _attackDamage;
+		
+	public:
+		ClapTrap();
+		ClapTrap(std::string const name);
+		ClapTrap(const ClapTrap &other);
+		ClapTrap &operator=(const ClapTrap &other);
+		~ClapTrap();
+		
+		std::string getName(void)const;
+		unsigned int getHitPoint(void)const;
+		unsigned int getEnergyPoint(void)const;
+		unsigned int getAttackDamage(void)const;
+		
+		void setHitPoint(unsigned int value);
+		void setEnergyPoint(unsigned int value);
+		
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		void run(void);
+		void actionWrapper(unsigned int value, int func);
+};
+
+#endif
